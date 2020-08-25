24ad540336dfa8346ee47f4dc1abaa8037320832
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2020-08-25 00:45:17 -0700

    finish basic implementation, TODO: Refactor to JS

--- flask-app/image_converter.py
+++ flask-app/image_converter.py
@@ -3,8 +3,8 @@ import imghdr
 
 def resize(input_path):
     img = Image.open(input_path)
-    if img.size[0] >= 500:
-        img = img.resize((500, 500)) 
+    if img.size[0] >= 200:
+        img = img.resize((200, 200)) 
     else:
         img = img.resize((round(img.size[0], -1), round(img.size[1], -1)))
     img.save(input_path, img.format)
